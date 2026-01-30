7cfca999bd7315c063a0e3292c68ea6123e95c88
Rob Snider <rsnider@wisecode.ai>
2026-01-30 12:16:32 -0500

    chore: Remove `util.service_role_key` Supabase migration and enable Android core library desugaring.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -95,4 +96,5 @@ flutter {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.10")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
